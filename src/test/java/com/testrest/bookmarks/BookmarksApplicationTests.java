64456ca8f3404a6400585225049d59e56b0b0f5f
Timothy Gerstel <tim.gerstel@gmail.com>
2018-06-08 11:41:41 -0400

    Archived quote app, added REST app boilerplate

--- src/test/java/com/testrest/bookmarks/BookmarksApplicationTests.java
+++ src/test/java/com/testrest/bookmarks/BookmarksApplicationTests.java
@@ -1,4 +1,4 @@
-package com.testapp.test;
+package com.testrest.bookmarks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestApplicationTests {
+public class BookmarksApplicationTests {
 
 	@Test
 	public void contextLoads() {
